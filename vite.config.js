bb4c9963f604e76caacf87a8e056064bd0e1149f
M.A. Asardeen <asardeenbse@gmail.com>
2025-07-04 01:09:35 +0530

    changes vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/weather-app/", // <-- add this line
+  // base: "/weather-app/", // <-- add this line
+  base: "/weather-app/", // important! repo name with slash
   plugins: [react()],
 });
